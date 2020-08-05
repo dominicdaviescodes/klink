3a80ca182ba4f57791e05121c6d58b48c346cd94
dominicdaviescodes <codewithdom@gmail.com>
2020-08-05 21:06:17 +0100

    edit gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-// Gulp.js configuration
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
@@ -6,59 +5,40 @@ var cleanCSS = require('gulp-clean-css');
 var browserSync = require('browser-sync').create();
 
 function update_html() {
-  return gulp
-    .src(['src/*.html'])
-    .pipe(gulp.dest('dest'))
-    .pipe(browserSync.stream());
+    return gulp.src(['src/*.html']).pipe(gulp.dest('dest')).pipe(browserSync.stream());
 }
 
 function update_scripts() {
-  return gulp
-    .src(['src/js/*.js'])
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('dest/js'))
-    .pipe(browserSync.stream());
+    return gulp.src(['src/js/*.js']).pipe(concat('main.js')).pipe(gulp.dest('dest/js')).pipe(browserSync.stream());
 }
 
 function update_styles() {
-  return gulp
-    .src(['src/scss/*.scss'])
-    .pipe(sass())
-    .on('error', function (err) {
-      console.log(err.toString());
-      this.emit('end');
-    })
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('dest/css'))
-    .pipe(browserSync.stream());
+    return gulp
+        .src(['src/scss/*.scss'])
+        .pipe(sass())
+        .on('error',function(err){
+            console.log(err.toString());
+            this.emit('end');
+        })
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('dest/css'))
+        .pipe(browserSync.stream());
 }
 
 function copy_files() {
-  return gulp
-    .src(['src/images/**', 'src/json/images.json'], {
-      base: 'src/'
-    })
-    .pipe(gulp.dest('dest'));
+    return gulp.src(['src/images/**', 'src/json/images.json'], { base: 'src/' }).pipe(gulp.dest('dest'));
 }
 
 function serve_browser() {
-  browserSync.init({
-    server: './dest'
-  });
+    browserSync.init({
+        server: './dest'
+    });
 
-  gulp.watch(['src/*.html']).on('change', update_html);
-  gulp.watch(['src/js/*.js']).on('change', update_scripts);
-  gulp.watch(['src/scss/*.scss']).on('change', update_styles);
+    gulp.watch(['src/*.html']).on('change', update_html);
+    gulp.watch(['src/js/*.js']).on('change', update_scripts);
+    gulp.watch(['src/scss/*.scss']).on('change', update_styles);
 }
 
-var build = gulp.series(
-  gulp.parallel(
-    copy_files,
-    update_scripts,
-    update_styles,
-    update_html,
-    serve_browser
-  )
-);
+var build = gulp.series(gulp.parallel(copy_files, update_scripts, update_styles, update_html, serve_browser));
 
 exports.default = build;
