a8166e652bc7dc5a745841ca0ee9be3effd16504
dominicdaviescodes <codewithdom@gmail.com>
2020-08-05 22:22:28 +0100

    add main js file and link in html file

---
+++ src/js/js-index.js
@@ -0,0 +1,9 @@
+var firstName = 'Dominic';
+console.log(firstName);
+
+function changeDivs() {
+  var e = document.getElementById('javascript_app');
+  // e.style.border = '3px dashed fuchsia';
+}
+
+changeDivs();
