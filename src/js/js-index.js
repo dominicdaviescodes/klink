fc6cbeae4444678deec991146304dac9eb08b5b3
dominicdaviescodes <codewithdom@gmail.com>
2020-08-06 20:48:02 +0100

    add functionality to next and previous buttons

--- src/js/js-index.js
+++ src/js/js-index.js
@@ -1,3 +1,25 @@
+function js_button(id, images) {
+  this.id = id;
+  this.mode = 'art';
+  this.images = images[this.mode];
+  this.count = 0;
+  this.total = this.images.length;
+  this.el = document.getElementById(id);
+  this.image_id = document.getElementById('js_image');
+  this.status = document.getElementById('js_badge');
+
+  this.el.addEventListener('click', this.swapImage.bind(this));
+}
+
+js_button.prototype.swapImage = function () {
+  alert(this.id);
+}
+
+function JS_APPInit(data) {
+  var btnPrev = new js_button('prev', data);
+  var btnNext = new js_button('next', data);
+}
+
 var request = new XMLHttpRequest();
 request.open('GET', 'json/images.json', true);
 
@@ -5,7 +27,8 @@ request.onload = function () {
   if (request.status >= 200 && request.status < 400) {
     // file is found and loaded
     var data = JSON.parse(request.responseText);
-    console.log(data);
+    // console.log(data);
+    JS_APPInit(data);
   } else {
     var lMessage = request.status + ' File not found!';
     alert(lMessage);
