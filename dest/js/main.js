98bf82ba6c027a983a8232bab368eb0aee524888
dominicdaviescodes <codewithdom@gmail.com>
2020-08-07 22:05:34 +0100

    add images json to vue app

--- dest/js/main.js
+++ dest/js/main.js
@@ -135,6 +135,25 @@ $.getJSON('json/images.json', function (data) {
 var vueApp = new Vue({
   el: '#vue_app',
   data: {
-    title: 'Vue.js'
+    title: 'Vue.js',
+    mode: 'animals'
+  },
+  methods: {
+    loadImageJson() {
+      fetch('./json/images.json')
+        .then(function (response) {
+          if (response.status !== 200) {
+            console.log('Fetch error:', response.status);
+            return;
+          }
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+        })
+    }
+  },
+  created() {
+    this.loadImageJson();
   }
 });
--- src/js/js-vue.js
+++ src/js/js-vue.js
@@ -1,6 +1,25 @@
 var vueApp = new Vue({
   el: '#vue_app',
   data: {
-    title: 'Vue.js'
+    title: 'Vue.js',
+    mode: 'animals'
+  },
+  methods: {
+    loadImageJson() {
+      fetch('./json/images.json')
+        .then(function (response) {
+          if (response.status !== 200) {
+            console.log('Fetch error:', response.status);
+            return;
+          }
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+        })
+    }
+  },
+  created() {
+    this.loadImageJson();
   }
 });
