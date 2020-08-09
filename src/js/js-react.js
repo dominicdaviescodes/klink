0e2f4f98b21c9c718bcc09fc93d8572ebafc564a
dominicdaviescodes <codewithdom@gmail.com>
2020-08-09 20:38:32 +0100

    add json data with fetch function

--- src/js/js-react.js
+++ src/js/js-react.js
@@ -1,21 +1,64 @@
 class ReactApp extends React.Component {
-  render() {
-    return ( <
-      div >
-      <
-      div className = "col-12 bg-dark" >
-      <
-      h1 > React < span > < /span> </h1 >
-      <
-      /div>
 
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      total: 0,
+      mode: 'nature',
+      images: [],
+      image: '',
+      credits: ''
+    }
+  }
+  componentDidMount() {
+    var self = this;
+    fetch('./json/images.json')
+      .then(function (response) {
+        if (response.status !== 200) {
+          console.log('Fetch error:', response.status);
+          return;
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        self.setState({
+          images: data. [self.state.mode]
+        });
+        self.setState({
+          image: data[self.state.mode][0].image
+        });
+        self.setState({
+          total: data[self.state.mode].length
+        });
+      })
+  }
+
+  render() {
+    return (
+      <div >
+      <div className = "col-12 bg-dark" >
+      <h1> {
+        ' ' }
+      React < span > < /span>< /
+      h1 > <
+      /div> <
+      div className = "col-12 p-0" >
       <
+      img src = ""
+      alt = ""
+      className = "img-fluid" / >
+      <
+      div className = "photo-credits" > < /div> < /
+      div > <
+      div className = "col bg-dark text-center p-2 button-controls" >
+      <
+      span className = "badge badge-light" > 4 < /span> < /
+      div > <
       /div>
-    )
+    );
   }
 }
 
-ReactDOM.render( <
-  ReactApp / > ,
-  document.getElementById('react_app')
-)
+ReactDOM.render( < ReactApp / > , document.getElementById('react_app'));
